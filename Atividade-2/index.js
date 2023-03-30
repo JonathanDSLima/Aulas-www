3f2be73230ea352444159986ca5a62803d3b098b
Jonathan de Souza Lima <jonathandesouzalima@MacBook-Air-de-Jonathan.local>
2023-03-29 22:21:13 -0300

    Envio da segunda atividade

--- Atividade-2/index.js
+++ Atividade-2/index.js
@@ -1,75 +1,85 @@
 $(document).ready(() => { 
-    popularTabela() 
-    $( ".search" ).on( "click", function() {
-        searchCoin($('.form-control').val())
-    });
+    fillTable() 
 })
 
 const url = "https://api.coincap.io/v2/assets";
 
-function popularTabela(field){
+function fillTable(symbol){
     $.ajax({
         url: url,
         type: "GET"
     })
     .done(function(obj) {
-        exibirCoins(obj.data);
+        if(symbol){
+            let aux = obj.data.find( t => t.symbol == symbol);
+            if (aux){
+                displayOneCoin(aux)
+            } else {  
+                $(".modal").modal("show")
+                $("#modal-message").html("Moeda inválida!")
+            }
+        } else {
+            displayCoins(obj.data);
+        }  
     })
-    .fail(function(message){
-        console.log(message);
+    .fail(function(msg){
+        const error = msg.responseJSON.message
+        console.log(error, msg);
+        if(error === "Not Found"){
+            $(".modal").modal("show")
+            $("#modal-message").html("Moeda não encontrada na base de dados oO")
+        }
     })
 }
 
-function exibirCoins(data){
-    for (let index = 0; index < data.length; index++) {
-        const link = `<a class='btn' target='_blank' href=${data[index].explorer}><img class='icon-link' src="images/link.png" alt="${data[index].explorer}"></a>`
-
-        $(".body-data").append(
-            "<tr>" +
-            "<td>" + data[index].symbol + "</td>" +
-            "<td>" + data[index].id.replace(/(^\w{1})|(\s+\w{1})/g, letra => letra.toUpperCase()) + "</td>" +
-            "<td>" + parseFloat(data[index].priceUsd).toFixed(4)  + "</td>" +
-            "<td>" + parseFloat(data[index].changePercent24Hr).toFixed(2)  + "%" + "</td>" +
-            "<td>" + link + "</td>" +
-            "</tr>"
-        )
+$( ".search" ).on( "click", function() {
+    const inputValue = $('.form-control').val().toUpperCase()
+    if(inputValue === ""){
+        $(".modal").modal("show")
+        $("#modal-message").html("O campo de pesquisa não pode ser vazio =/")
+    } else {
+        fillTable(inputValue)
     }
-}
+});
 
-function searchCoin(field){
-    $.ajax({
-        url: url,
-        type: "GET"
-    })
-    .done(function(obj) {
-        let aux = obj.data.find( t => t.symbol == field);
-        $.ajax({
-            url: url + '/' + aux.id,
-            type: "GET"
-        })
-        .done(function(obj) {
-            displayOneCoin(obj);
-        })
-        .fail(function(message){
-            console.log(message);
-        })
-    })
-    .fail(function(message){
-        console.log(message);
-    })
+$( ".clear" ).on( "click", function() {
+    fillTable()
+});
+
+function displayCoins(data){
+    clearTable()
+    for (let index = 0; index < data.length; index++) {
+        constructTable(data[index])
+    }
 }
 
 function displayOneCoin(obj){
     console.log(obj);
+    clearTable()
+    constructTable(obj)
+}
+
+function clearTable(){
     $(".body-data" ).remove();
     $("table" ).append('<tbody class="body-data"></tbody>');
-    const link = `<a class='btn' target='_blank' href=${obj.data.explorer}><img class='icon-link' src="images/link.png" alt="${obj.data.explorer}"></a>`
+}
+
+function constructTable(obj){
+    const formatter = new Intl.NumberFormat('pt-br', {
+        style: 'currency',
+        currency: 'BRL',
+      });
+    const link = `<a class='btn' target='_blank' href=${obj.explorer}><img class='icon-link' src="images/link.png" alt="${obj.explorer}"></a>`
+    const changePercent = obj.changePercent24Hr 
+    let color = "green";
+    changePercent > 0 ? color : color = "red"
+
     $(".body-data").append(
         "<tr>" +
-        "<td>" + obj.data.symbol + "</td>" +
-        "<td>" + obj.data.id.replace(/(^\w{1})|(\s+\w{1})/g, letra => letra.toUpperCase()) + "</td>" +
-        "<td>" + parseFloat(obj.data.priceUsd).toFixed(4)  + "</td>" +
-        "<td>" + parseFloat(obj.data.changePercent24Hr).toFixed(2)  + "%" + "</td>" +
+        "<td>" + obj.symbol + "</td>" +
+        "<td>" + obj.name + "</td>" +
+        "<td>" + formatter.format(parseFloat(obj.priceUsd).toFixed(4))  + "</td>" +
+        `<td style='color: ${color}'>` + parseFloat(obj.changePercent24Hr).toFixed(2)  + "%" + "</td>" +
         "<td>" + link + "</td>" +
         "</tr>"
     )
